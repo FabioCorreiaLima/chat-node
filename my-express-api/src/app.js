6106ea43d61ce3a7ea1ceef31b385d5eac6f7089
Fábio Correia Lima <118375746+FabioCorreiaLima@users.noreply.github.com>
2024-10-10 19:06:37 -0300

    Update app.js

--- my-express-api/src/app.js
+++ my-express-api/src/app.js
@@ -11,18 +11,18 @@ const routerUser = require('./routes/rotas_user')
 
 const app = express();
 
-// Middleware para CORS e JSON
+
 app.use(cors());
 app.use(express.json());
 
 // Conectar ao MongoDB
 connectMongoDB();
 
-// Servir arquivos estáticos na pasta 'public'
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('', routerUser)
-// Roteamento
+
 app.use('/auth', authRoutes);
 app.use('/api', roomRoutes);
 
